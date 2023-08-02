31599bf147925607c6ca67925d93ee9a43969e85
gautam-bhagat <gautambhagat.business@gmail.com>
2023-08-02 19:44:17 +0530

    database changed

--- database-connection/connection.js
+++ database-connection/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const mongoURI = 'mongodb+srv://quickjot-admin:QuickJotAdmin@cluster0.ycliqbl.mongodb.net/?retryWrites=true&w=majority';
-const mongoURI = 'mongodb://127.0.0.1:27017/QuickJot?directConnection=true';
+const mongoURI = 'mongodb+srv://quickjot-admin:QuickJotAdmin@cluster0.ycliqbl.mongodb.net/?retryWrites=true&w=majority';
+// const mongoURI = 'mongodb://127.0.0.1:27017/QuickJot?directConnection=true';
 
 const connectToMongo = () =>{
     mongoose.connect(mongoURI,{}).then(()=>{
@@ -8,5 +8,5 @@ const connectToMongo = () =>{
         
     }).catch(err => console.log(err))
 }
-
+ 
 module.exports = connectToMongo;
